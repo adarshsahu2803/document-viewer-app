d09b107a2803cddb347a3cf82beac59827b02059
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-27 17:26:37 +0530

    Added toolbar frame

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import './App.css'
+import DocumentViewer from './components/DocumentViewer.js'
+import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
+import dummyPDF from './assets/dummy.pdf';
 
 function App() {
+  // const docs = [
+  //   { uri: require(dummyPDF) },
+  // ];
+
   return (
-    <h1 className="text-3xl font-bold">
-      Document Viewer App
-    </h1>
+    <div>
+      <h1 className='font-bold text-2xl'>Document Viewer App</h1>
+      <DocumentViewer />
+      {/* <DocViewer
+        pluginRenderers={DocViewerRenderers}
+        documents={docs}
+        config={{
+          header: {
+            disableHeader: false,
+            disableFileName: false,
+            retainURLParams: false,
+          }
+        }}
+        style={{ height: 1000 }}
+      /> */}
+    </div>
   );
 }
 
